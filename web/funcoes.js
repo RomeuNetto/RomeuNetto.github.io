4b196f6889d6b4f5ff0c392df95372b141e04d7b
RomeuNetto <105459710+RomeuNetto@users.noreply.github.com>
2023-04-06 12:19:23 -0300

    Add files via upload

--- web/funcoes.js
+++ web/funcoes.js
@@ -1,21 +1,23 @@
-    var x=document.getElementById('x').value;
-    var y=document.getElementById('y').value;
-    var z=document.getElementById('z').value;
 function soma(){
-    var resposta1=document.getElementById('resposta1');
+    let x=document.getElementById('x').value;
+    let y=document.getElementById('y').value;
+    let resposta1=document.getElementById('resposta1');
     resposta1.innerHTML= x+y;
     }
 function maior(){
-    var resposta2=document.getElementById('resposta2');
+    let x=document.getElementById('x').value;
+    let y=document.getElementById('y').value;
+    let resposta2=document.getElementById('resposta2');
     if(x>y) resposta2.innerHTML="x>y";
     else if(y>x) resposta2.innerHTML="y>x";
     else resposta2.innerHTML="x=y";
 }
 function primo(){
-    var resposta3=document.getElementById('resposta3');
-    var divisores=0;
+    let x=document.getElementById('x').value;
+    let resposta3=document.getElementById('resposta3');
+    let divisores=0;
 
-    for(var count=1 ; count<=numero ; count++)
+    for(let count=1 ; count<=numero ; count++)
     {
      if(x % count == 0)
          divisores++;
@@ -28,24 +30,33 @@ function primo(){
     
 }
 function calcHipotenusa(){
-    var resposta4= document.getElementById('resposta4');
+    let x=document.getElementById('x').value;
+    let y=document.getElementById('y').value;
+    let resposta4= document.getElementById('resposta4');
     var hipotenusa= Math.sqrt(Math.pow(x)+Math.pow(y));
     resposta4=hipotenusa;
 }
 function newSalario(){
-    var resposta5= document.getElementById('resposta5');
-    var valorReajuste= x * (y / 100);
-    var novoSalario= x+valorReajuste;
+    let x=document.getElementById('x').value;
+    let y=document.getElementById('y').value;
+    let resposta5= document.getElementById('resposta5');
+    let valorReajuste= x * (y / 100);
+    let novoSalario= x+valorReajuste;
     resposta5.innerHTML= novoSalario;
 }
 function transTemperatura(){
-    var resposta6= document.getElementById('resposta6');
+    let x=document.getElementById('x').value;
+    let y=document.getElementById('y').value;
+    let resposta6= document.getElementById('resposta6');
     y=((x-32)/9);
     y=y/5;
     resposta6.innerHTML=y;
 }
 function mediaFinal(){
-    var resposta7= document.getElementById('resposta7');
-    var medFinal=(x*2+y*3+z*5)/10;
+    let x=document.getElementById('x').value;
+    let y=document.getElementById('y').value;
+    let z=document.getElementById('z').value;
+    let resposta7= document.getElementById('resposta7');
+    let medFinal=(x*2+y*3+z*5)/10;
     resposta7.innerHTML=medFinal;
 }
